3b54f532da03f9c1a19c777d870c48b5eb01b21e
Velescore <42907171+Velescore@users.noreply.github.com>
2018-10-02 02:08:26 +0200

    typo

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2774,7 +2774,7 @@ bool CWallet::CreateTransaction(const vector<pair<CScript, CAmount> >& vecSend,
                         strFailReason = _("Insufficient funds.");
                     } else if (coin_type == ONLY_NOT10000IFMN) {
                         strFailReason = _("Unable to locate enough funds for this transaction that are not equal 1200 VLS.");
-                    } else if (coin_type == ONLY_NONDENOMINATED_NOT1000IFMN) {
+                    } else if (coin_type == ONLY_NONDENOMINATED_NOT10000IFMN) {
                         strFailReason = _("Unable to locate enough Obfuscation non-denominated funds for this transaction that are not equal 10000 VLS.");
                     } else {
                         strFailReason = _("Unable to locate enough Obfuscation denominated funds for this transaction.");
